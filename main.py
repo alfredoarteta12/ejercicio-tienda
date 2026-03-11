c8e63f0a91f65c44f2fde3477802826764951f66
alfredo arteta <alfredoarteta03@gmail.com>
2026-03-10 19:38:11 -0500

    codigo final

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 ventas = []
 
-
-
-producto = input("ingrese nombre del producto: ")
-precio = int(input("ingrese precio del producto: "))
-cantidad = int(input("ingrese cantidad del producto: "))
-
 def registrar_venta():
-    venta = {"producto": producto, "precio": precio, "cantidad": cantidad}
-    ventas.append(venta)
 
-valor_venta = precio * cantidad
-print(f"el valor de la venta es : {valor_venta}")
-
-registrar_nueva_venta = input("desea registar nueva venta si/no:")
-while registrar_nueva_venta.lower() == "si":
     producto = input("ingrese nombre del producto: ")
     precio = int(input("ingrese precio del producto: "))
     cantidad = int(input("ingrese cantidad del producto: "))
-    registrar_venta()
+
+    venta = {"producto": producto, "precio": precio, "cantidad": cantidad}
+    ventas.append(venta)
 
     valor_venta = precio * cantidad
     print(f"el valor de la venta es : {valor_venta}")
+
+
+registrar_venta()
+
+registrar_nueva_venta = input("desea registar nueva venta si/no:")
+
+while registrar_nueva_venta.lower() == "si":
+    registrar_venta()
     registrar_nueva_venta = input("desea registar nueva venta si/no:")
 
 
+print("\nRESUMEN DE VENTAS")
+
+for venta in ventas:
+    print("producto:", venta["producto"])
+    print("cantidad:", venta["cantidad"])
+
+
+total = 0
+
+for venta in ventas:
+    total += venta["precio"] * venta["cantidad"]
+
+print("Total vendido:", total)
+print("Gracias por usar el programa de ventas.")
